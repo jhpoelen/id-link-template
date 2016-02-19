08890fe4d4579fbb5f9fee892fddd1de41ae111f
gaurav <gaurav@ggvaidya.com>
2016-02-19 14:23:26 -0700

    Increased limit.

--- mediawiki/wikicommons_dwc_triplet.pl
+++ mediawiki/wikicommons_dwc_triplet.pl
@@ -7,6 +7,7 @@ use warnings;
 # CONFIGURATION
 our $BASE_URL = 'http://commons.wikimedia.org/w/index.php';
 our $API_URL = 'http://commons.wikimedia.org/w/api.php';
+our $SETS_OF_50 = 10_000;
 
 # Step 1. Identify keywords.
 use Getopt::Long;
@@ -22,10 +23,13 @@ say STDERR "Searching $API_URL for '$query'";
 
 # Step 2. Search the mediawiki instance we want.
 use MediaWiki::API;
-my $mw = MediaWiki::API->new();
-$mw->{config}->{api_url} = $API_URL;
+my $mw = MediaWiki::API->new({
+    api_url => $API_URL,
+    retries => $SETS_OF_50   
+});
 
-my $count = 0;
+my $count_results = 0;
+my $count_matches = 0;
 
 $mw->list({
     action => 'query',
@@ -35,15 +39,16 @@ $mw->list({
     srwhat => 'text',
     srlimit => 50
 }, {
-    max => 200, # Real max = max (200) * srlimit (50)   
+    max => $SETS_OF_50, # Real max = max (200) * srlimit (50)   
     hook => sub {
         my ($results) = @_;
         my @results = @{$results};
+        my $result_count = scalar @results;
             
-        say STDERR "Retrieved $#results results, $count already processed.";
-        $count += $#results;
+        say STDERR "Extracted $count_matches matches from $count_results processed articles and files.";
+        $count_results += $result_count;
 
-        if($count < $offset) {
+        if($count_results < $offset) {
             return;
         }
 
@@ -69,12 +74,15 @@ $mw->list({
             
             # DwC Triplet regex based on http://journals.plos.org/plosone/article?id=10.1371/journal.pone.0114069
             if(defined($content) and ($content ne '')) {
-                my @matches = ($content =~ /([A-Z]{3,8}[\:\s](?:[A-Z][a-z]+[\:\s])?[0-9\.]+)/);
+                my @matches = ($content =~ /\b([A-Z]{3,8}[\:\s](?:[A-Za-z]+[\:\s])?[0-9\.]+\.?)\b/);
 
                 foreach my $match (@matches) {
                     say "$page_url,refers,$match";
+                    $count_matches++;
                 }
             }
         }
     }
 }) or die $mw->{error}->{code} . ': ' . $mw->{error}->{details};
+
+say STDERR "Extracted $count_matches matches from $count_results processed rows.";
